3a45c78a7f5dae8768c45c889f45ba590dab8352
Don40 <drklukes@gmail.com>
2024-12-02 22:26:37 +0530

    i made changes forasjg;js.

--- app/about/page.js
+++ app/about/page.js
@@ -13,7 +13,7 @@ const page = () => {
             <SecondaryHero
                 bgImage="/images/bg-image.webp"
                 text="ABOUT US!"
-                paragraph="Our prayer is that you will be blessed and strengthened by the power of Jesus, and that you will live a life of abundance in fellowship, joy and liberty. The River of Life Church is here for you. It is not just a church; it is a fellowship of believers coming together to declare the glory of the Lord, and to celebrate Jesus as King. We study the Word, practice what we learn, and in the process grow together. May God richly bless you! May the Lord bless you and keep you! We hope to see you soon!"
+                paragraph="Our prayer asd;glkjs;ldgj;sdjhg;sis that you will be blessed and strengthened by the power of Jesus, and that you will live a life of abundance in fellowship, joy and liberty. The River of Life Church is here for you. It is not just a church; it is a fellowship of believers coming together to declare the glory of the Lord, and to celebrate Jesus as King. We study the Word, practice what we learn, and in the process grow together. May God richly bless you! May the Lord bless you and keep you! We hope to see you soon!"
             />
             <AboutUs/>
 
